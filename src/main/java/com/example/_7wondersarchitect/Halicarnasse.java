b37c5baa42f105756fa8ff90728902a7000acf4a
Martin-JONCOURT <115220121+Martin-JONCOURT@users.noreply.github.com>
2023-01-20 10:47:47 +0100

    Update Halicarnasse.java

--- src/main/java/com/example/_7wondersarchitect/Halicarnasse.java
+++ src/main/java/com/example/_7wondersarchitect/Halicarnasse.java
@@ -2,9 +2,9 @@ package com.example._7wondersarchitect;
 
 public class Halicarnasse extends Wonders{
     String nom = "Halicarnasse";
-    int NBEtage = 5;
     int NBPiece = 5;
-    public Halicarnasse(String nom, int NBEtage, int NBPiece) {
-        super(nom, NBEtage, NBPiece);
+    int NBPiececonstru;
+    public Halicarnasse(String nom, int NBPiece, int NBPiececonstru) {
+        super(nom, NBPiece, NBPiececonstru);
     }
 }
