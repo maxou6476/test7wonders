4b9c3b9d7e8898ec5c753905f0c23a4e283328be
Martin-JONCOURT <115220121+Martin-JONCOURT@users.noreply.github.com>
2023-01-20 10:48:18 +0100

    Update Rhodes.java

--- src/main/java/com/example/_7wondersarchitect/Rhodes.java
+++ src/main/java/com/example/_7wondersarchitect/Rhodes.java
@@ -2,9 +2,9 @@ package com.example._7wondersarchitect;
 
 public class Rhodes extends Wonders{
     String nom = "Rhodes";
-    int NBEtage = 5;
     int NBPiece = 5;
-    public Rhodes(String nom, int NBEtage, int NBPiece) {
-        super(nom, NBEtage, NBPiece);
+    int NBPiececonstru;
+    public Rhodes(String nom, int NBPiece, int NBPiececonstru) {
+        super(nom, NBPiece, NBPiececonstru);
     }
 }
