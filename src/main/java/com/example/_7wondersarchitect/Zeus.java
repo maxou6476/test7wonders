be3a1ea3aae5653369d84c7cbb29a71b75dc5739
Martin-JONCOURT <115220121+Martin-JONCOURT@users.noreply.github.com>
2023-01-20 10:50:09 +0100

    Create Zeus.java

---
+++ src/main/java/com/example/_7wondersarchitect/Zeus.java
@@ -0,0 +1,10 @@
+package com.example._7wondersarchitect;
+
+public class Zeus extends Wonders{
+    String nom = "Zeus";
+    int NBPiece = 5;
+    int NBPiececonstru;
+    public Zeus(String nom, int NBPiece, int NBPiececonstru) {
+        super(nom, NBPiece, NBPiececonstru);
+    }
+}
