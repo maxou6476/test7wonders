01cf71f6e41c7512c07b0d6bb2c9f7565bfe5aed
SofianeKETTOU <115222554+SofianeKETTOU@users.noreply.github.com>
2023-01-20 12:17:16 +0100

    Create Deck.java

---
+++ src/main/java/com/example/_7wondersarchitect/Deck.java
@@ -0,0 +1,34 @@
+package com.example._7wondersarchitect;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Cartes> cards;
+
+    public Deck() {
+        cards = new ArrayList<Cartes>();
+    }
+
+    public void addCard(Cartes card) {
+        cards.add(card);
+    }
+
+    public Cartes drawCard() {
+        if (cards.size() > 0) {
+            return cards.remove(0);
+        } else {
+            return null;
+        }
+    }
+    public ArrayList<Cartes> getDeck() {
+        return cards;
+    }
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public int getNumberOfCards() {
+        return cards.size();
+    }
+}
